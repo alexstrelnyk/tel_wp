9d45490fe6eba09e699c141a1ae8b184435c9584
alexstrelnyk <o.strelnyk@telesens.ua>
2024-01-23 17:28:34 +0200

    TS-2 Fixed code formatting

--- themes/twentytwentyfour/header.php
+++ themes/twentytwentyfour/header.php
@@ -1,20 +1,27 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8"/>
-        <link rel="icon" href="/favicon.ico"/>
-        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no"/>
-        <meta name="theme-color" content="#004d35"/>
-        <meta name="description" content="Telesens global solutions"/>
-        <link rel="apple-touch-icon" href="/logo192.png"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BKQBLFP3ZT"></script>
-        <script>function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-BKQBLFP3ZT")</script>
-        <title>Telesens</title>
-        <script defer="defer" src="<?php echo path('js', 'main.js')?>"></script>
-        <link href="<?php echo path('css', 'style.css')?>" rel="stylesheet">
-    </head>
-    <body>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
-        
-        <main id="root">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+    <meta name="theme-color" content="#004d35" />
+    <meta name="description" content="Telesens global solutions" />
+    <link rel="apple-touch-icon" href="/logo192.png" />
+    <link rel="manifest" href="/manifest.json" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BKQBLFP3ZT"></script>
+    <script>
+        function gtag() {
+            dataLayer.push(arguments)
+        }
+        window.dataLayer = window.dataLayer || [], gtag("js", new Date), gtag("config", "G-BKQBLFP3ZT")
+    </script>
+    <title>Telesens</title>
+    <script defer="defer" src="<?php echo path('js', 'main.js') ?>"></script>
+    <link href="<?php echo path('css', 'style.css') ?>" rel="stylesheet">
+</head>
+
+<body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+
+    <main id="root">
