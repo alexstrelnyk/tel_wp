09f70a626cf2b8f73752510402f7b357aca1d487
alexstrelnyk <o.strelnyk@telesens.ua>
2024-02-01 16:51:35 +0200

    TS-4 Added home menu feature

--- themes/twentytwentyfour/assets/js/index.js
+++ themes/twentytwentyfour/assets/js/index.js
@@ -4,5 +4,20 @@ $(document).ready(function(){
         $('#splash-screen').hide();
         $('#root').show();
     }, 3000);
-    console.log('page loaded');
+
+    
+    
+});
+$('.menu-bar').click(function(){
+    var menu_btn_pos = $('#header-menu .menu-bar').offset();
+    $('.main-menu .menu').css({
+        'top': menu_btn_pos.top + 'px',
+        'left': menu_btn_pos.left + 'px'
+    });
+
+    if ($('.main-menu').hasClass('visible')) {
+        $('.main-menu').removeClass('visible');
+    } else {
+        $('.main-menu').addClass('visible');
+    }
 });
