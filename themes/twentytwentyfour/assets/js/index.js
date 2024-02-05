6924b89760bcfd0c2518b783ea0b4ff6378e515c
alexstrelnyk <o.strelnyk@telesens.ua>
2024-02-05 20:12:23 +0200

    TS-12 Fixed animated counters when user scroll down

--- themes/twentytwentyfour/assets/js/index.js
+++ themes/twentytwentyfour/assets/js/index.js
@@ -1,14 +1,12 @@
 $('#root').hide();
-$(document).ready(function(){
-    setTimeout(function(){
+$(document).ready(function () {
+    setTimeout(function () {
         $('#splash-screen').hide();
         $('#root').show();
     }, 3000);
-
-    
-    
 });
-$('.menu-bar').click(function(){
+
+$('.menu-bar').click(function () {
     var menu_btn_pos = $('#header-menu .menu-bar').offset();
     $('.main-menu .menu').css({
         'top': menu_btn_pos.top + 'px',
@@ -23,3 +21,57 @@ $('.menu-bar').click(function(){
         $('#header-menu .menu-check').addClass('active');
     }
 });
+
+var element = document.getElementById('cursor');
+var rect = element.getBoundingClientRect();
+function animateCount(target, duration, element) {
+    var start = 0;
+    var increment = 10;
+    var steps = duration / increment;
+    var currentStep = 0;
+
+    var stepValue = target / steps;
+
+    function updateCount() {
+        start += stepValue;
+        $(element).text(Math.round(start));
+
+        currentStep++;
+        if (currentStep < steps) {
+            requestAnimationFrame(updateCount);
+        }
+    }
+
+    updateCount();
+}
+function runCounters() {
+    $('.regular').each(function () {
+        var target = parseInt($(this).data('count'));
+        animateCount(target, 1500, this);
+    });
+}
+
+function isInViewport(element) {
+    if (!element || typeof element.getBoundingClientRect !== 'function') {
+      return false;
+    }
+  
+    var rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+function handleScroll() {
+    var targetSection = $('#sec_r3');
+
+    if (isInViewport(targetSection[0])) {
+      runCounters();
+      $(window).off('scroll', handleScroll);
+    }
+  }
+
+  $(window).on('scroll', handleScroll);
