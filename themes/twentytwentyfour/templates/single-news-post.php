728e5194f7dfecd69166290ced818b68f07246aa
alexstrelnyk <o.strelnyk@telesens.ua>
2024-02-08 14:06:50 +0200

    TS-13 Fixed blog page structure

--- themes/twentytwentyfour/templates/single-news-post.php
+++ themes/twentytwentyfour/templates/single-news-post.php
@@ -10,7 +10,11 @@ get_header();
 ?>
 <section data=":r3:" id="blog-section" class="blog-view frc bg-soft-blue">
     <div class="container">
-        <div class="blog-side-bar"><img loading="lazy" src="<?php the_post_thumbnail_url(); ?>" alt="<?php echo basename(get_the_post_thumbnail_url()) ?>" class="mb12"></div>
+        <div class="blog-side-bar">
+            <?php if (has_post_thumbnail()) { ?>
+                <img loading="lazy" src="<?php the_post_thumbnail_url(); ?>" alt="<?php echo basename(get_the_post_thumbnail_url()) ?>" class="mb12">
+            <?php } ?>
+        </div>
         <div class="flex-col blog-content">
             <?php echo get_post_field('post_content', get_queried_object_id()); ?>
             <div class="block-spacer" style="height: 0px;"></div>
