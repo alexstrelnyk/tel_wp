a78dc0ce691854de36f85bdeb30ec6676169f989
alexstrelnyk <o.strelnyk@telesens.ua>
2024-02-08 13:58:27 +0200

    TS-13 Fixed blog page structure

--- themes/twentytwentyfour/templates/single-post.php
+++ themes/twentytwentyfour/templates/single-post.php
@@ -11,6 +11,9 @@ get_header();
 <section data=":r3:" id="blog-section" class="blog-view frc bg-soft-blue">
     <div class="container">
         <div class="blog-side-bar">
+            <?php if (has_post_thumbnail()) { ?>
+                <img loading="lazy" src="<?php the_post_thumbnail_url()?>" alt="<?php echo basename(get_the_post_thumbnail_url()) ?>" class="mb12">
+            <?php } ?>
             <p class="Sub2 color-black  mb12">
                 <?php echo get_field('description'); ?>
             </p>
@@ -28,7 +31,7 @@ get_header();
             </p>
         </div>
         <div class="flex-col blog-content">
-            <?php echo get_post_field('post_content', get_queried_object_id());?>
+            <?php echo get_post_field('post_content', get_queried_object_id()); ?>
             <div class="block-spacer" style="height: 0px;"></div>
         </div>
     </div>
