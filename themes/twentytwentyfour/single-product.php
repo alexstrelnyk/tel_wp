371366a19576057274f076e320fb2ec85db8c844
alexstrelnyk <o.strelnyk@telesens.ua>
2024-02-23 17:29:17 +0200

    TS-7 Fixed each 2nd level of categories bg

--- themes/twentytwentyfour/single-product.php
+++ themes/twentytwentyfour/single-product.php
@@ -1,20 +1,16 @@
 <div class="flex-col sub_parent sub_parent_<?php echo $level ?>">
-    <div id="cards-tree" class="shelf-content bg-light-blue"></div>
-    <div class="slider-container bg-light-blue">
-        <div class="slider-side-bar bg-light-blue">
+    <div id="cards-tree" class="shelf-content <?php echo $level % 2 === 0 ? 'bg-yellow' : 'bg-light-blue' ?>"></div>
+    <div class="slider-container <?php echo $level % 2 === 0 ? 'bg-yellow' : 'bg-light-blue' ?>">
+        <div class="slider-side-bar <?php echo $level % 2 === 0 ? 'bg-yellow' : 'bg-light-blue' ?>">
             <p class="Sub color-black "><?php echo $cat_title ?></p>
         </div>
         <div class="services-slider" style="transform: translateX(0px);">
             <?php
             foreach ($categories as $key => $category) {
-                $label = '';
-                if ($key == 0) {
-                    $label = 'selected';
-                }
                 $image = get_field('category_image', 'category_' . $category->cat_ID);
             ?>
 
-                <div class="services-card yellow <?php echo $label ?>" onclick="getProducts(this, <?php echo $level ?>)" data-cat_id="<?php echo $category->cat_ID ?>" data-cat_title="<?php echo $category->cat_name ?>">
+                <div class="services-card <?php echo $level % 2 === 0 ? 'light-blue' : 'yellow' ?>" onclick="getProducts(this, <?php echo $level ?>)" data-cat_id="<?php echo $category->cat_ID ?>" data-cat_title="<?php echo $category->cat_name ?>">
                     <?php if ($image) { ?>
                         <div class="image"><img loading="lazy" src="<?php echo $image['url'] ?>" alt="<?php echo $image['name'] ?>"></div>
                     <?php } ?>
