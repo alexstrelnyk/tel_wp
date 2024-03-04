7aec0d5c0960b86a558fe10b040e1fc3b26b038b
alexstrelnyk <o.strelnyk@telesens.ua>
2024-03-04 17:40:59 +0200

    TS-7 Fixed bug with swiper js loading

--- themes/twentytwentyfour/footer.php
+++ themes/twentytwentyfour/footer.php
@@ -80,8 +80,7 @@
             </footer>
             </main>
             <script defer="defer" src="<?php echo path('js', 'cursor.js') ?>"></script>
-
-            <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+            <script src="<?php echo path('js', 'swiper-bundle.min.js') ?>"></script>
 
             <script>
                 var swiper = new Swiper(".mySwiper");
