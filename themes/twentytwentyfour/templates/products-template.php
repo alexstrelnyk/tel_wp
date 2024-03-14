0385918160ce73c5bd2749e09426854d4758c3c9
alexstrelnyk <o.strelnyk@telesens.ua>
2024-03-14 15:53:18 +0200

    TS-7 Added products page en lang

--- themes/twentytwentyfour/templates/products-template.php
+++ themes/twentytwentyfour/templates/products-template.php
@@ -10,11 +10,11 @@ get_header();
     <div class="services-tree">
         <div class="flex-col sub_parent_0">
             <div id="cards-tree" class="shelf-content bg-midnight-blue">
-                <p class="H3 color-white  italic">Продукти та Послуги</p>
+                <p class="H3 color-white  italic"><?php echo get_label('Продукти та Послуги', 'Products & Services') ?></p>
             </div>
             <div class="slider-container bg-midnight-blue">
                 <div class="slider-side-bar bg-midnight-blue">
-                    <p class="Sub color-white ">НАШІ ПРОДУКТИ ТА ПОСЛУГИ</p>
+                    <p class="Sub color-white "><?php echo get_label('НАШІ ПРОДУКТИ ТА ПОСЛУГИ', 'OUR PRODUCTS AND SERVICES') ?></p>
                 </div>
                 <div class="services-slider" style="transform: translateX(0px);">
 
@@ -72,25 +72,25 @@ get_header();
     <div class="contact-us bg-white">
         <div class="contact-form-root">
             <div class="text-input" data-cursor="input-text">
-                <p class="Sub2 color-black ">Ім'я*</p><input type="text" class="input" name="firstName" enterkeyhint="next" value="">
+                <p class="Sub2 color-black "><?php echo get_label('Ім\'я', 'First name') ?>*</p><input type="text" class="input" name="firstName" enterkeyhint="next" value="">
             </div>
             <div class="text-input" data-cursor="input-text">
-                <p class="Sub2 color-black ">Прізвище</p><input type="text" class="input" name="lastName" enterkeyhint="next" value="">
+                <p class="Sub2 color-black "><?php echo get_label('Прізвище', 'Last name') ?></p><input type="text" class="input" name="lastName" enterkeyhint="next" value="">
             </div>
             <div class="text-input" data-cursor="input-text">
-                <p class="Sub2 color-black ">Номер телефону*</p><input type="text" class="input" name="phone" enterkeyhint="next" value="">
+                <p class="Sub2 color-black "><?php echo get_label('Номер телефону', 'Phone number') ?>*</p><input type="text" class="input" name="phone" enterkeyhint="next" value="">
             </div>
             <div class="text-input" data-cursor="input-text">
                 <p class="Sub2 color-black ">E-mail*</p><input type="text" class="input" name="email" enterkeyhint="next" value="">
             </div>
             <div class="text-input" data-cursor="input-text">
-                <p class="Sub2 color-black ">Повідомлення</p><textarea maxlength="2000" class="input multi" name="content" enterkeyhint="enter" style="height: 118px;"></textarea>
+                <p class="Sub2 color-black "><?php echo get_label('Повідомлення', 'Message') ?></p><textarea maxlength="2000" class="input multi" name="content" enterkeyhint="enter" style="height: 118px;"></textarea>
                 <p class="Body color-just-grey letters-counter">0/2000</p>
             </div>
             <div class="checkbox-root" data-cursor="active"><label class="checkbox path"><input type="checkbox" value="false"><svg viewBox="0 0 21 21">
                         <path d="M5,10.75 L8.5,14.25 L19.4,2.3 C18.8333333,1.43333333 18.0333333,1 17,1 L4,1 C2.35,1 1,2.35 1,4 L1,17 C1,18.65 2.35,20 4,20 L17,20 C18.65,20 20,18.65 20,17 L20,7.99769186"></path>
                     </svg></label><a href="privacy-policy" target="_blank" rel="noopener noreferrer">
-                    <p class="Body color-just-grey regular Link">Я погоджуюся з Політикою конфіденційності</p>
+                    <p class="Body color-just-grey regular Link"><?php echo get_label('Я погоджуюся з Політикою конфіденційності', 'I agree with the Privacy Policy') ?></p>
                 </a></div>
             <div class="submission frc">
                 <div class="btn-box undefined">
