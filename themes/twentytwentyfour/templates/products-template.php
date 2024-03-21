ac2128d971e10e53fe5a54d58f76c797dbfd7762
alexstrelnyk <o.strelnyk@telesens.ua>
2024-03-21 19:30:00 +0200

    TS-17 Fixed auto height feature for products feedbacks

--- themes/twentytwentyfour/templates/products-template.php
+++ themes/twentytwentyfour/templates/products-template.php
@@ -107,39 +107,42 @@ get_header();
     </div>
 </section>
 
-<section id=":r1a:" class="width-wrapper">
-    <div class="quotes bg-soft-blue swiper mySwiper">
-        <div class="quotes-slider swiper-wrapper" data-cursor="slider">
-            <?php
-            $category = get_category_by_slug(pll_current_language() == 'uk' ? 'client_feedback' : 'client_feedback-en');
+<section id=":r9:" class="width-wrapper">
+    <div class="quotes bg-soft-blue">
+        <div class="quotes bg-soft-blue swiper mySwiper">
+            <div class="quotes-slider swiper-wrapper" data-cursor="slider">
+                <?php
+                $category = get_category_by_slug(pll_current_language() == 'uk' ? 'client_feedback' : 'client_feedback-en');
 
-            if ($category) {
-                $args = array(
-                    'category__in' => array($category->term_id),
-                    'posts_per_page' => -1,
-                );
-                $posts_query = new WP_Query($args);
+                if ($category) {
+                    $args = array(
+                        'category__in' => array($category->term_id),
+                        'posts_per_page' => -1,
+                    );
+                    $posts_query = new WP_Query($args);
 
-                if ($posts_query->have_posts()) {
-                    while ($posts_query->have_posts()) {
-                        $posts_query->the_post();
+                    if ($posts_query->have_posts()) {
+                        while ($posts_query->have_posts()) {
+                            $posts_query->the_post();
 
-            ?>
-                        <div class="quote-container swiper-slide"><?php the_content() ?></div>
-            <?php
+                ?>
+                            <div class="quote-container swiper-slide"><?php the_content() ?></div>
+                <?php
+                        }
+                        wp_reset_postdata();
                     }
-                    wp_reset_postdata();
                 }
-            }
-            ?>
+                ?>
+            </div>
         </div>
     </div>
 </section>
+
+
 <script>
     var parentCatSlugs = false;
     var postId = false;
 </script>
-
 
 <?php
 
