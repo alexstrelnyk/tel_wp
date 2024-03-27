cc54824c86505cef20d390e1c37419f0bba1b517
alexstrelnyk <o.strelnyk@telesens.ua>
2024-03-27 16:27:46 +0200

    TS-7 Moved "planets" to parts

---
+++ themes/twentytwentyfour/parts/planets.php
@@ -0,0 +1,55 @@
+<section id=":r6:" class="width-wrapper">
+    <div class="blog-preview frc bg-white">
+        <div class="container">
+            <div class="side f3 flex column">
+                <p class="Sub2 color-black "><?php echo get_label('Співпраця', 'Collaborations') ?></p>
+            </div>
+            <div class="side f7 flex column">
+                <p class="H3 color-black spacer italic"><?php echo get_label('Партнери та клієнти', 'Partners and clients') ?></p>
+                <p class="Body color-black "></p>
+                <p class="Body color-black "><?php echo get_label('Компанії, з якими ми маємо честь працювати', 'Companies, we are privileged to work with') ?></p>
+            </div>
+            <div class="f2"></div>
+        </div>
+    </div>
+</section>
+
+<section id=":r7:" class="width-wrapper">
+    <div class="bg-white partners-section">
+        <div class="partners">
+            <svg class="motion-path" width="1920" height="1046" viewBox="0 0 1920 1046" fill="none">
+                <g clip-path="url(#clip0_64_845)">
+                    <path d="M1210.95 523.437C1210.95 555.668 1183.05 585.001 1137.59 606.305C1092.17 627.592 1029.38 640.771 960 640.771C890.621 640.771 827.835 627.592 782.412 606.305C736.949 585.001 709.052 555.668 709.052 523.437C709.052 491.207 736.949 461.874 782.412 440.569C827.835 419.283 890.621 406.104 960 406.104C1029.38 406.104 1092.17 419.283 1137.59 440.569C1183.05 461.874 1210.95 491.207 1210.95 523.437Z" stroke="#03a65a" id="motion-path-0"></path>
+                    <path d="M1336.15 523.438C1336.15 571.937 1294.24 616 1226.12 647.966C1158.04 679.914 1063.95 699.688 960 699.688C856.049 699.688 761.963 679.914 693.884 647.966C625.765 616 583.854 571.937 583.854 523.438C583.854 474.938 625.765 430.875 693.884 398.909C761.963 366.961 856.049 347.188 960 347.188C1063.95 347.188 1158.04 366.961 1226.12 398.909C1294.24 430.875 1336.15 474.938 1336.15 523.438Z" stroke="#03a65a" id="motion-path-1" opacity="0.6"></path>
+                    <path d="M1466.6 524.49C1466.6 589.549 1410.33 648.603 1318.98 691.422C1227.67 734.222 1101.5 760.708 962.104 760.708C822.709 760.708 696.534 734.222 605.227 691.422C513.881 648.603 457.604 589.549 457.604 524.49C457.604 459.43 513.881 400.376 605.227 357.557C696.534 314.757 822.709 288.271 962.104 288.271C1101.5 288.271 1227.67 314.757 1318.98 357.557C1410.33 400.376 1466.6 459.43 1466.6 524.49Z" stroke="#03a65a" id="motion-path-2" opacity="0.5"></path>
+                    <path d="M1592.85 523.438C1592.85 605.057 1522.21 679.103 1407.64 732.774C1293.1 786.427 1134.84 819.625 960 819.625C785.161 819.625 626.898 786.427 512.363 732.774C397.789 679.103 327.146 605.057 327.146 523.438C327.146 441.818 397.789 367.772 512.363 314.101C626.898 260.448 785.161 227.25 960 227.25C1134.84 227.25 1293.1 260.448 1407.64 314.101C1522.21 367.772 1592.85 441.818 1592.85 523.438Z" stroke="#03a65a" id="motion-path-3" opacity="0.4"></path>
+                    <path d="M1718.05 523.438C1718.05 621.326 1633.4 710.102 1496.17 774.435C1358.97 838.749 1169.41 878.542 960 878.542C750.588 878.542 561.026 838.749 423.835 774.435C286.604 710.102 201.948 621.326 201.948 523.438C201.948 425.549 286.604 336.773 423.835 272.44C561.026 208.126 750.588 168.333 960 168.333C1169.41 168.333 1358.97 208.126 1496.17 272.44C1633.4 336.773 1718.05 425.549 1718.05 523.438Z" stroke="#03a65a" id="motion-path-4" opacity="0.3"></path>
+                    <path d="M1844.3 523.438C1844.3 637.596 1745.51 741.101 1585.44 816.095C1425.4 891.071 1204.27 937.458 960 937.458C715.725 937.458 494.6 891.071 334.563 816.095C174.485 741.101 75.6979 637.596 75.6979 523.438C75.6979 409.279 174.485 305.774 334.563 230.78C494.6 155.804 715.725 109.417 960 109.417C1204.27 109.417 1425.4 155.804 1585.44 230.78C1745.51 305.774 1844.3 409.279 1844.3 523.438Z" stroke="#03a65a" id="motion-path-5" opacity="0.2"></path>
+                    <path d="M1969.5 523.438C1969.5 653.865 1856.7 772.099 1673.97 857.756C1491.27 943.393 1238.85 996.375 960 996.375C681.153 996.375 428.728 943.393 246.034 857.756C63.3006 772.099 -49.5 653.865 -49.5 523.438C-49.5 393.01 63.3006 274.776 246.034 189.119C428.728 103.482 681.153 50.5 960 50.5C1238.85 50.5 1491.27 103.482 1673.97 189.119C1856.7 274.776 1969.5 393.01 1969.5 523.438Z" stroke="#03a65a" id="motion-path-6" opacity="0.1"></path>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M1008.77 465.876C1042.69 465.066 1072.34 467.902 1089.8 472.964C1072.95 463.649 1049.79 456.561 1022.78 452.511C1008.16 458.384 1002.27 463.851 1008.77 465.876Z" fill="#03a65a"></path>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M807.905 535.945C807.5 535.337 807.297 534.932 806.892 534.325C807.095 534.932 807.5 535.337 807.905 535.945Z" fill="#03a65a"></path>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M1100.77 480.054C1085.54 472.561 1046.75 468.713 1003.08 471.346C948.652 474.586 906.611 486.737 909.658 498.28C912.704 509.418 956.167 516.101 1007.75 513.671C1005.52 513.873 1003.29 514.076 1001.05 514.076C939.31 517.721 886.301 510.228 882.849 497.065C879.396 484.104 926.515 470.333 987.85 466.688C991.506 462.435 1003.49 456.36 1018.92 452.107C1000.04 449.475 979.523 448.462 957.792 449.272C870.663 452.31 800.797 483.699 802.016 519.543C802.219 524.201 803.64 528.859 806.078 533.112C817.654 545.87 862.742 554.173 917.172 552.755C981.554 550.932 1035.17 536.149 1036.8 519.543C1038.22 503.748 991.1 492.002 930.171 492.609C932.811 492.407 935.451 492.407 938.091 492.407C1010.8 490.382 1068.89 504.153 1067.06 522.784C1065.43 539.187 1019.33 553.97 959.214 558.83C955.152 564.703 943.778 571.994 928.546 578.474C939.513 579.081 950.683 579.081 962.26 578.879C1049.59 575.841 1119.46 544.452 1118.24 508.608C1118.24 498.077 1111.94 488.357 1100.77 480.054Z" fill="#03a65a"></path>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M930.784 560.25C880.416 561.465 837.156 555.187 816.44 545.062C835.937 562.073 873.916 574.223 919.817 577.868C933.424 569.97 938.095 563.085 930.784 560.25Z" fill="#03a65a"></path>
+                </g>
+            </svg>
+            <div class="partner-list">
+                <div class="partner dynamic pos-0 line-0 offset-0" data-cursor="active"><a href="https://ucell.uz/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/1676384661951-l1.png" alt=""></a></div>
+                <div class="partner dynamic pos-1 line-1 offset-1" data-cursor="active"><a href="https://kyivstar.ua/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/57231fa9-fb4d-4b86-9c8a-c8b3bdaf091aimage 72.png" alt=""></a></div>
+                <div class="partner dynamic pos-2 line-2 offset-2" data-cursor="active"><a href="https://vega.ua/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/2f0189f7-33a6-4557-8330-58a1f664813dimage 74.png" alt=""></a></div>
+                <div class="partner dynamic pos-3 line-3 offset-3" data-cursor="active"><a href="https://ukrtelecom.ua/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/5124694f-6725-45da-9767-e7e7dbea0e11image 77.png" alt=""></a></div>
+                <div class="partner dynamic pos-4 line-4 offset-4" data-cursor="active"><a href="https://www.kcell.kz/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/9fece58a-8056-471e-bb7f-20861e07ae94image 78.png" alt=""></a></div>
+                <div class="partner dynamic pos-5 line-5 offset-5" data-cursor="active"><a href="https://beeline.uz/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/1676030146193-an.png" alt=""></a></div>
+                <div class="partner dynamic pos-6 line-6 offset-6" data-cursor="active"><a href="https://career.vodafone.ua/it-smartflex" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/7eb376cf-ee3b-460e-8c62-4b2d1e3df4c1image 81.png" alt=""></a></div>
+                <div class="partner dynamic pos-7 line-0 offset-7" data-cursor="active"><a href="https://www.vodafone.ua/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/a850870d-ec7c-43ce-97cc-f6cfbfd171c3image 83.png" alt=""></a></div>
+                <div class="partner dynamic pos-8 line-1 offset-8" data-cursor="active"><a href="https://telecom.kz/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/c8ba895e-0c1d-4f92-930e-8545b6881855image 84.png" alt=""></a></div>
+                <div class="partner dynamic pos-9 line-2 offset-9" data-cursor="active"><a href="https://www.jusanmobile.kz/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/1676276950561-go.png" alt=""></a></div>
+                <div class="partner dynamic pos-10 line-3 offset-0" data-cursor="active"><a href="https://www.nokia.com/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/1676629856247-ns.png" alt=""></a></div>
+                <div class="partner dynamic pos-11 line-4 offset-1" data-cursor="active"><a href="https://tele2.kz/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/adcbf938-e6ef-4a57-82d1-7f98dc2faeceimage 79.png" alt=""></a></div>
+                <div class="partner dynamic pos-12 line-5 offset-2" data-cursor="active"><a href="https://www.velton.ua/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="/wp-content/themes/twentytwentyfour/assets/images/1676557818689-go.png" alt=""></a></div>
+                <div class="partner dynamic pos-13 line-6 offset-3"><a href="https://www.velton.ua/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="https://telesens.ua/admin/files/upload/1676557818689-go.png" alt=""></a></div>
+
+                <div class="partner dynamic pos-14 line-0 offset-4"><a href="https://www.ericsson.com/" target="_blank" rel="noopener noreferrer"><img loading="lazy" src="https://telesens.ua/admin/files/upload/1707407943790-go.png" alt=""></a></div>
+            </div>
+        </div>
+    </div>
+</section>
