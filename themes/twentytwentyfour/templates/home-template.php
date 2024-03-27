019e7d8f0dc6c03d301e59a006f63269c44e2567
alexstrelnyk <o.strelnyk@telesens.ua>
2024-03-27 12:17:18 +0200

    TS-7 Added translations

--- themes/twentytwentyfour/templates/home-template.php
+++ themes/twentytwentyfour/templates/home-template.php
@@ -14,7 +14,7 @@ echo get_post_field('post_content', get_queried_object_id());
 <section id=":r5:" class="width-wrapper">
     <div class="bg-grey-blue slider-container">
         <div class="slider-side-bar bg-midnight-blue">
-            <p class="Sub color-white  bold">ПРОДУКТИ І ПОСЛУГИ</p>
+            <p class="Sub color-white  bold"><?php echo get_label('ПРОДУКТИ І ПОСЛУГИ', 'PRODUCTS AND SERVICES') ?></p>
         </div>
         <div class="services-slider">
             <div class="swiper" id="products_swiper">
@@ -41,7 +41,16 @@ echo get_post_field('post_content', get_queried_object_id());
 
                                 <div class="product-card">
                                     <div class="product-desc">
-                                        <div class="product-image" style="transform: rotate(0.584795deg);" data-cursor="slider-img" onclick="goto('<?php echo get_permalink($product_page->ID) ?>?cat_slug=<?php echo $category->slug ?>')"><img loading="lazy" src="<?php echo $image['url'] ?>" alt="<?php echo $category->name ?>"></div><a class="Body animated-link" data-cursor="active" href="<?php echo get_permalink($product_page->ID) ?>?cat_id=<?php echo $category->cat_ID ?>"><span class="title"><span data-text="Читати більше" class="color-after-bright-green color-before-white">Читати більше</span></span></a>
+                                        <div class="product-image" style="transform: rotate(0.584795deg);" data-cursor="slider-img" onclick="goto('<?php echo get_permalink($product_page->ID) ?>?cat_slug=<?php echo $category->slug ?>')">
+                                            <img loading="lazy" src="<?php echo $image['url'] ?>" alt="<?php echo $category->name ?>">
+                                        </div>
+                                        <a class="Body animated-link" data-cursor="active" href="<?php echo get_permalink($product_page->ID) ?>?cat_id=<?php echo $category->cat_ID ?>">
+                                            <span class="title">
+                                                <span data-text="<?php echo get_label('Читати більше', 'Show more') ?>" class="color-after-bright-green color-before-white">
+                                                    <?php echo get_label('Читати більше', 'Show more') ?>
+                                                </span>
+                                            </span>
+                                        </a>
                                     </div>
                                     <div class="pager" data-cursor="slider-white"></div>
                                     <div class="product-title">
