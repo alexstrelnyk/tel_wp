5a1906555538378b04047db61371b01969145a8f
alexstrelnyk <o.strelnyk@telesens.ua>
2024-04-01 16:02:16 +0300

    TS-7 Fixed contact form layout

--- themes/twentytwentyfour/parts/form.php
+++ themes/twentytwentyfour/parts/form.php
@@ -24,35 +24,11 @@
 
 <?php } ?>
 
+
+
 <section data=":rq:" id="contact-us" class="width-wrapper">
     <div class="contact-us bg-<?php echo is_contact_page() ? 'gu-light-green' : 'white' ?>">
-        <div class="contact-form-root">
-            <?php echo do_shortcode('[contact-form-7 id="8039dfd" title="Contact form 1"]') ?>
-
-            <div class="checkbox-root" data-cursor="active">
-                <label class="checkbox path">
-                    <input type="checkbox" value="false">
-                    <svg viewBox="0 0 21 21">
-                        <path d="M5,10.75 L8.5,14.25 L19.4,2.3 C18.8333333,1.43333333 18.0333333,1 17,1 L4,1 C2.35,1 1,2.35 1,4 L1,17 C1,18.65 2.35,20 4,20 L17,20 C18.65,20 20,18.65 20,17 L20,7.99769186"></path>
-                    </svg>
-                </label>
-                <a href="privacy-policy" target="_blank" rel="noopener noreferrer">
-                    <p class="Body color-just-grey regular Link">I agree with the Privacy Policy</p>
-                </a>
-            </div>
-            <div class="submission frc">
-                <div class="btn-box undefined">
-                    <div class="btn-joint" style="transition: all 0.75s ease-out 0s;" data-cursor="active">
-                        <div class="svg-btn send-btn"></div>
-                    </div>
-                </div>
-                <div class="clutch pulse" data-cursor="active" to="https://clutch.co/profile/telesens#reviews">
-                    <a href="https://clutch.co/profile/telesens#reviews" target="_blank" rel="noopener noreferrer">
-                        <div class="svg-btn clutch-btn"></div>
-                    </a>
-                </div>
-            </div>
-        </div>
+        <?php echo do_shortcode('[contact-form-7 id="8039dfd" title="Contact form 1"]') ?>
     </div>
 </section>
 
