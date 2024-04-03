f5df677266f7bfe0436af5474c2c1bb3ec3e6486
alexstrelnyk <o.strelnyk@telesens.ua>
2024-04-03 17:02:58 +0300

    TS-7 Added "post format definer" to products

--- themes/twentytwentyfour/single-product-post.php
+++ themes/twentytwentyfour/single-product-post.php
@@ -4,10 +4,18 @@
 <div class="sub_parent sub_parent_<?php echo $level ?>">
     <section data=":r7:" id="blog-section" class="blog-view frc bg-soft-blue">
         <div class="container">
-            <div class="blog-side-bar"></div>
-            <div class="flex-col blog-content">
-                <?php echo $content ?>
-            </div>
+            <?php
+            if (strstr($content, 'class="container"') !== false) {
+                echo $content;
+            } else {
+            ?>
+                <div class="blog-side-bar"></div>
+                <div class="flex-col blog-content">
+                    <?php echo $content ?>
+                </div>
+            <?php
+            }
+            ?>
         </div>
     </section>
 </div>
