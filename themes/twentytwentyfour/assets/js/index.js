b16daa48db353de1e3fc67789d71497ae2956bd6
alexstrelnyk <o.strelnyk@telesens.ua>
2024-04-05 17:46:38 +0300

    TS-31 Added swiper feature to gallery

--- themes/twentytwentyfour/assets/js/index.js
+++ themes/twentytwentyfour/assets/js/index.js
@@ -341,6 +341,7 @@ function initSlider(selectorId) {
 
 initSlider('feedback_swiper');
 initSlider('products_swiper');
+initSlider('gallery_swiper');
 
 function get_label(uk, en) {
     if (lang == 'en') {
