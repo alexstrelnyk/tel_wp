77f5f27991432feaf3ef819cec3f63db2c5c56dd
alexstrelnyk <o.strelnyk@telesens.ua>
2024-04-09 17:41:05 +0300

    TS-7 Added "our-values" part for "about-us" page

---
+++ themes/twentytwentyfour/parts/our-values.php
@@ -0,0 +1,64 @@
+<section id=":r5:" class="root-values bg-white">
+    <div class="sticky values-header center bg-white" style="top: 60px;">
+        <div id="inner-blog-section" class="container inner-blog-preview">
+            <div class="side f3 flex-column">
+                <p class="Body color-black "></p>
+            </div>
+            <div class="side f7 flex column">
+                <p class="H3 color-black  italic"><?php echo get_label('Наші цінності', 'Our values') ?></p>
+                <p class="Body color-black "></p>
+            </div>
+        </div>
+    </div>
+    <div class="spacer"></div>
+    <div class="container up-2 sticky" style="top: 190px;">
+        <div class="single-card">
+            <div class="card-spacer" style="height: 240px;"></div>
+            <div class="sticky" style="top: 190px;">
+                <div class="value-card bg-navy-green">
+                    <div class="card-content">
+                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/72940c30-f145-4a15-8cc5-d8f530f5d5a8-Ellipse67.png&quot;);"></div>
+                        <p class="Sub color-white value-title"><?php echo get_label('Професіоналізм', 'Professionalism') ?>.</p>
+                        <p class="Body2 color-white "><?php echo get_label('Ми використовуємо наш досвід і компетентність для досягнення амбітних цілей наших клієнтів', 'We translate our experience and competence into achieving ambitious goals for our clients') ?>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-card">
+            <div class="card-spacer" style="height: 0px;"></div>
+            <div class="sticky" style="top: 190px;">
+                <div class="value-card bg-navy-green">
+                    <div class="card-content">
+                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/044d9dee-41d6-42aa-a7e8-503e3d6a7e19-Ellipse681.png&quot;);"></div>
+                        <p class="Sub color-white value-title"><?php echo get_label('Люди', 'People') ?>.</p>
+                        <p class="Body2 color-white "><?php echo get_label('Успіх кожного покращує нас', 'The success of each one improves us') ?>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-card">
+            <div class="card-spacer" style="height: 140px;"></div>
+            <div class="sticky" style="top: 190px;">
+                <div class="value-card bg-navy-green">
+                    <div class="card-content">
+                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/943b676c-e89c-484a-8ec5-47d28461c643-Ellipse69.png&quot;);"></div>
+                        <p class="Sub color-white value-title"><?php echo get_label('Наполегливість', 'Persistence') ?>.</p>
+                        <p class="Body2 color-white "><?php echo get_label('Найкращі результати досягаються завдяки наполегливості та відданості справі', 'Best results come with persistence and commitment') ?>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-card">
+            <div class="card-spacer" style="height: 300px;"></div>
+            <div class="sticky" style="top: 190px;">
+                <div class="value-card bg-navy-green">
+                    <div class="card-content">
+                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/fb365d2a-5a82-4f91-b67e-3ea1aee7102a-Ellipse701.png&quot;);"></div>
+                        <p class="Sub color-white value-title"><?php echo get_label('Сприйняття', 'Perception') ?>.</p>
+                        <p class="Body2 color-white "><?php echo get_label('Ми адаптуємося, щоб досягати виняткових результатів і задовольнити всі явні та неявні потреби', 'We adapt to deliver exceptional results and meet all explicit and implicit needs') ?>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- themes/twentytwentyfour/templates/about-us-template.php
+++ themes/twentytwentyfour/templates/about-us-template.php
@@ -9,7 +9,6 @@ echo get_post_field('post_content', get_queried_object_id());
 get_template_part('parts/gallery-slider');
 ?>
 
-
 <section id="sec_r3" class="width-wrapper">
     <div class="counter full-width row flex center bg-soft-blue">
         <div class="container">
@@ -35,73 +34,9 @@ get_template_part('parts/gallery-slider');
     </div>
 </section>
 
-<section id=":r5:" class="root-values bg-white">
-    <div class="sticky values-header center bg-white" style="top: 60px;">
-        <div id="inner-blog-section" class="container inner-blog-preview">
-            <div class="side f3 flex-column">
-                <p class="Body color-black "></p>
-            </div>
-            <div class="side f7 flex column">
-                <p class="H3 color-black  italic">Наші цінності</p>
-                <p class="Body color-black "></p>
-            </div>
-        </div>
-    </div>
-    <div class="spacer"></div>
-    <div class="container up-2 sticky" style="top: 190px;">
-        <div class="single-card">
-            <div class="card-spacer" style="height: 240px;"></div>
-            <div class="sticky" style="top: 190px;">
-                <div class="value-card bg-navy-green">
-                    <div class="card-content">
-                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/72940c30-f145-4a15-8cc5-d8f530f5d5a8-Ellipse67.png&quot;);"></div>
-                        <p class="Sub color-white value-title">Професіоналізм.</p>
-                        <p class="Body2 color-white ">Ми використовуємо наш досвід і компетентність для досягнення амбітних цілей наших клієнтів.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="single-card">
-            <div class="card-spacer" style="height: 0px;"></div>
-            <div class="sticky" style="top: 190px;">
-                <div class="value-card bg-navy-green">
-                    <div class="card-content">
-                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/044d9dee-41d6-42aa-a7e8-503e3d6a7e19-Ellipse681.png&quot;);"></div>
-                        <p class="Sub color-white value-title">Люди.</p>
-                        <p class="Body2 color-white ">Успіх кожного покращує нас.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="single-card">
-            <div class="card-spacer" style="height: 140px;"></div>
-            <div class="sticky" style="top: 190px;">
-                <div class="value-card bg-navy-green">
-                    <div class="card-content">
-                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/943b676c-e89c-484a-8ec5-47d28461c643-Ellipse69.png&quot;);"></div>
-                        <p class="Sub color-white value-title">Наполегливість.</p>
-                        <p class="Body2 color-white ">Найкращі результати досягаються завдяки наполегливості та відданості справі.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="single-card">
-            <div class="card-spacer" style="height: 300px;"></div>
-            <div class="sticky" style="top: 190px;">
-                <div class="value-card bg-navy-green">
-                    <div class="card-content">
-                        <div class="value-circle" style="background-image: url(&quot;/wp-content/themes/twentytwentyfour/assets/images/fb365d2a-5a82-4f91-b67e-3ea1aee7102a-Ellipse701.png&quot;);"></div>
-                        <p class="Sub color-white value-title">Сприйняття.</p>
-                        <p class="Body2 color-white ">Ми адаптуємося, щоб досягати виняткових результатів і задовольнити всі явні та неявні потреби.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-
 <?php
+get_template_part('parts/our-values');
+
 get_template_part('parts/products-slider');
 
 get_template_part('parts/planets');
