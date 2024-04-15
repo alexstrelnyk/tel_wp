0a45e93c8a698c29a3502aa7988de604b800c009
alexstrelnyk <o.strelnyk@telesens.ua>
2024-04-15 15:34:05 +0300

    TS-7 Added link to blog post image

--- themes/twentytwentyfour/templates/clean-post.php
+++ themes/twentytwentyfour/templates/clean-post.php
@@ -9,7 +9,7 @@ get_header();
 
 ?>
 <section data=":r3:" id="blog-section" class="blog-view frc bg-soft-blue">
-    <div class="container">
+    <div class="container" style="white-space: pre-line">
         <?php echo get_post_field('post_content', get_queried_object_id()); ?>
     </div>
 </section>
