7e0d38acf35a5f3e03d2a6e615f359f08584e822
alexstrelnyk <o.strelnyk@telesens.ua>
2024-04-15 13:44:47 +0300

    TS-7 Fixed post template

---
+++ themes/twentytwentyfour/templates/clean-post.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template Name: Clean post
+ * Template Post Type: post
+ */
+
+get_header();
+
+?>
+<section data=":r3:" id="blog-section" class="blog-view frc bg-soft-blue">
+    <div class="container">
+        <?php echo get_post_field('post_content', get_queried_object_id()); ?>
+    </div>
+</section>
+
+<?php
+
+get_footer();
