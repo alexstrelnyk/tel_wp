b9f80a1ae7e4fe0bd4a43102f32d638feeb0774c
alexstrelnyk <o.strelnyk@telesens.ua>
2024-04-17 15:26:39 +0300

    TS-7 Added post_id quick get

--- themes/twentytwentyfour/single-product-post.php
+++ themes/twentytwentyfour/single-product-post.php
@@ -1,7 +1,7 @@
 <script>
     var postId = '<?php echo $post->post_name ?>';
 </script>
-<div class="sub_parent sub_parent_<?php echo $level ?>">
+<div class="sub_parent sub_parent_<?php echo $level ?>" data-post_id="<?php echo $post->ID ?>">
     <section data=":r7:" id="blog-section" class="blog-view frc bg-soft-blue">
         <div class="container">
             <?php
