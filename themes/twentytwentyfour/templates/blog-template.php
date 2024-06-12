5bcf8fe5412d4c7f65b84cd72d136d0d46d43023
Dmytro Shatov <d.shatov@telesens.ua>
2024-06-12 10:31:02 +0300

    TS-59 Fixed cursor issue on the Blog page

--- themes/twentytwentyfour/templates/blog-template.php
+++ themes/twentytwentyfour/templates/blog-template.php
@@ -10,12 +10,12 @@ get_header();
     <div class="container">
         <div class="blog-side-bar">
             <div class="">
-                <p class="Sub color-black  mv12"><?php echo get_label('Закладки', 'Bookmarks') ?></p><a class="flex-row align-center gap8 mb12 one-tab" href="#ТЕЛЕСЕНС ІНСАЙТИ">
+                <p class="Sub color-black  mv12"><?php echo get_label('Закладки', 'Bookmarks') ?></p><a class="flex-row align-center gap8 mb12 one-tab" data-cursor="active" href="#ТЕЛЕСЕНС ІНСАЙТИ">
                     <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8.33203 0H1.66536C0.932031 0 0.338698 0.6 0.338698 1.33333L0.332031 12L4.9987 10L9.66537 12V1.33333C9.66537 0.6 9.06536 0 8.33203 0Z" fill="#004D35"></path>
                     </svg>
                     <p class="Body color-grey "><?php echo get_label('ТЕЛЕСЕНС ІНСАЙТИ', 'TELESENS INSIGHTS') ?></p>
-                </a><a class="flex-row align-center gap8 mb12 one-tab" href="#ТЕЛЕСЕНС НОВИНИ">
+                </a><a class="flex-row align-center gap8 mb12 one-tab" data-cursor="active" href="#ТЕЛЕСЕНС НОВИНИ">
                     <svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8.33203 0H1.66536C0.932031 0 0.338698 0.6 0.338698 1.33333L0.332031 12L4.9987 10L9.66537 12V1.33333C9.66537 0.6 9.06536 0 8.33203 0Z" fill="#004D35"></path>
                     </svg>
@@ -47,13 +47,13 @@ get_header();
                 setup_postdata($post);
             ?>
                 <div class="module-container type-linkedImage">
-                    <a href="<?php the_permalink(); ?>" target="_blank">
+                    <a href="<?php the_permalink(); ?>" data-cursor="active" target="_blank">
                         <img loading="lazy" src="<?php the_post_thumbnail_url(); ?>" alt="<?php echo basename(get_the_post_thumbnail_url()) ?>">
                     </a>
                 </div>
                 <div class="module-container type-paragraph">
                     <p class="Body color-  text-left">
-                        <a href="<?php the_permalink(); ?>" target="_blank" class="inh Link"><?php the_title(); ?></a>
+                        <a href="<?php the_permalink(); ?>" data-cursor="active" target="_blank" class="inh Link"><?php the_title(); ?></a>
                         <br>
                         <br>
                         <?php echo get_field('description'); ?>
@@ -73,11 +73,11 @@ get_header();
                 <p class="Body color-grey  text-left"><span class="inh  H4"><?php echo get_label('ТЕЛЕСЕНС НОВИНИ', 'TELESENS NEWS') ?></span></p>
             </div>
             <div class="module-container type-paragraph" style="white-space: pre-line;">
-                <p class="Body color-  text-left"><a href="<?php echo get_url('charity') ?>" target="_blank" class="inh Link"><?php echo get_label('Благодійність', 'Charity') ?></a>
+                <p class="Body color-  text-left"><a href="<?php echo get_url('charity') ?>" target="_blank" data-cursor="active" class="inh Link"><?php echo get_label('Благодійність', 'Charity') ?></a>
 
-                    ● <a href="https://helpingdoctors.telesens.co.uk/ua/?lang=ua" target="_blank" class="inh Link"><?php echo get_label('Допомога лікарям', 'Helping doctors') ?></a>
-                    ● <a href="https://helpingschoolchildren.telesens.co.uk" target="_blank" class="inh Link"><?php echo get_label('Допомога школярам', 'Helping schoolchildren') ?></a>
-                    ● <a href="https://helpingangels.telesens.co.uk/?lang=ua" target="_blank" class="inh Link"><?php echo get_label('Допомога янголам', 'Helping Angels') ?></a></p>
+                    ● <a href="https://helpingdoctors.telesens.co.uk/ua/?lang=ua" target="_blank" data-cursor="active" class="inh Link"><?php echo get_label('Допомога лікарям', 'Helping doctors') ?></a>
+                    ● <a href="https://helpingschoolchildren.telesens.co.uk" target="_blank" data-cursor="active" class="inh Link"><?php echo get_label('Допомога школярам', 'Helping schoolchildren') ?></a>
+                    ● <a href="https://helpingangels.telesens.co.uk/?lang=ua" target="_blank" data-cursor="active" class="inh Link"><?php echo get_label('Допомога янголам', 'Helping Angels') ?></a></p>
             </div>
 
             <?php
@@ -97,7 +97,7 @@ get_header();
                 <div class="module-container type-paragraph">
                     <p class="Body color-  text-left">
 
-                        <a href="<?php the_permalink(); ?>" target="_blank" class="inh Link"><?php the_title(); ?></a>
+                        <a href="<?php the_permalink(); ?>" target="_blank" data-cursor="active" class="inh Link"><?php the_title(); ?></a>
                         <br>
                         <br>
                         <?php echo get_field('description'); ?>
