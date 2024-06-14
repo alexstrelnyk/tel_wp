ee6e1c3fabc67ad4bbaff6441c431f56ab33ef9e
alexstrelnyk <o.strelnyk@telesens.ua>
2024-06-14 17:08:26 +0300

    TS-32 Fixed blog, charity and helping-defenders pages for mobile view

--- themes/twentytwentyfour/templates/blog-template.php
+++ themes/twentytwentyfour/templates/blog-template.php
@@ -54,13 +54,13 @@ get_header();
                 <div class="module-container type-paragraph">
                     <p class="Body color-  text-left">
                         <a href="<?php the_permalink(); ?>" data-cursor="active" target="_blank" class="inh Link"><?php the_title(); ?></a>
-                        <br>
-                        <br>
+                        <!-- <br>
+                        <br> -->
                         <?php echo get_field('description'); ?>
                     </p>
                 </div>
-                <br>
-                <br>
+                <!-- <br>
+                <br> -->
             <?php
             }
             wp_reset_postdata();
