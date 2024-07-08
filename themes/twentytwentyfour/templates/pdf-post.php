0ff87ba5c3ca10fc9477321d65e3f62cc8f41c77
Volodymyr Kurpita <v.kurpita@telesens.ua>
2024-07-08 18:44:29 +0300

    fix cursor style on posts with PDF files

--- themes/twentytwentyfour/templates/pdf-post.php
+++ themes/twentytwentyfour/templates/pdf-post.php
@@ -20,7 +20,7 @@ if (!$pdf = get_field('image_link')) {
     <div class="flex-col content">
         <div class="flex-row justify-between">
             <a href="<?php echo get_field('back_button_url') ?>">
-                <div class="btn">
+                <div class="btn" data-cursor="active">
                     <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7.41 10.59L2.83 6L7.41 1.41L6 0L0 6L6 12L7.41 10.59Z" fill="#004D35"></path>
                     </svg>
@@ -28,7 +28,7 @@ if (!$pdf = get_field('image_link')) {
                 </div>
             </a>
             <a href="<?php echo $pdf ?>" download="">
-                <div class="btn download">
+                <div class="btn download" data-cursor="active">
                     <p class="Sub2 color-navy-green ">Save file</p><svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M15.5837 6.75H11.2503V0.25H4.75033V6.75H0.416992L8.00033 14.3333L15.5837 6.75ZM0.416992 16.5V18.6667H15.5837V16.5H0.416992Z" fill="#AFBCBA"></path>
                     </svg>
