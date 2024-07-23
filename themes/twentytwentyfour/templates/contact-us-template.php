fa453bfe0e5299ff0843e66792d6f60d3b9fd3b0
Dmytro Shatov <d.shatov@telesens.ua>
2024-07-23 13:42:35 +0300

    TS-134 remove empty tags

--- themes/twentytwentyfour/templates/contact-us-template.php
+++ themes/twentytwentyfour/templates/contact-us-template.php
@@ -15,10 +15,8 @@ echo get_post_field('post_content', get_queried_object_id());
             </div>
             <div class="side f7 flex column">
                 <p class="H3 color-black spacer italic"><?php echo get_label('Зв\'яжіться з нами!', 'Get in touch!') ?></p>
-                <p class="Body color-black "></p>
                 <p class="Body color-black "><?php echo get_label('Давайте працювати разом! Розкажіть більше про свій проєкт', 'Let’s work together! Tell us more about your project') ?></p>
             </div>
-            <div class="f2"> </div>
         </div>
     </div>
 </section>
