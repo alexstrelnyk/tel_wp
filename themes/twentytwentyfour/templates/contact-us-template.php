fdabf221d2ecae24aaaba67c9166764184805d40
Dmytro Shatov <d.shatov@telesens.ua>
2024-07-25 12:00:35 +0300

    DS/TS-134 Remove paragraph if mob view

--- themes/twentytwentyfour/templates/contact-us-template.php
+++ themes/twentytwentyfour/templates/contact-us-template.php
@@ -11,7 +11,7 @@ echo get_post_field('post_content', get_queried_object_id());
     <div class="blog-preview frc bg-gu-light-green">
         <div class="container">
             <div class="side f3 flex column">
-                <p class="Sub2 color-black "> </p>
+                <p class="Sub2 color-black hide-mob"> </p>
             </div>
             <div class="side f7 flex column">
                 <p class="H3 color-black spacer italic"><?php echo get_label('Зв\'яжіться з нами!', 'Get in touch!') ?></p>
