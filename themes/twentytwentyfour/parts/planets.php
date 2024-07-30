a738fb83403c8c60e273e272787597a439c77e95
alexstrelnyk <o.strelnyk@telesens.ua>
2024-07-30 14:18:13 +0300

    TS-121 Fixed planets block for mobile view

--- themes/twentytwentyfour/parts/planets.php
+++ themes/twentytwentyfour/parts/planets.php
@@ -97,7 +97,21 @@ if (!in_array(get_page_slug(), ['about-us', 'about-us-en'])) {
         </div>
     </div>
 </section>
-
+<section id="planets_mobile" class="width-wrapper">
+    <div class="blog-preview frc bg-white">
+        <div class="container">
+            <div class="side f3 flex column">
+                <p class="Sub2 color-black "><?php echo get_label('Співпраця', 'Collaborations') ?></p>
+            </div>
+            <div class="side f7 flex column">
+                <p class="H3 color-black spacer italic"><?php echo get_label('Партнери та клієнти', 'Partners and clients') ?></p>
+                <p class="Body color-black "></p>
+                <p class="Body color-black "><?php echo get_label('Компанії, з якими ми маємо честь працювати', 'Companies, we are privileged to work with') ?></p>
+            </div>
+            <div class="f2"></div>
+        </div>
+    </div>
+</section>
 <section id="planets_mobile" class="width-wrapper">
     <div class="bg-white partners-mob">
         <div class="hinge" style="transform: rotate(360deg);"></div>
