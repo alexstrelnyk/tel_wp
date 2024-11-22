48dfd0117b8fb5e099138e121d2bf67a68dce300
alexstrelnyk <o.strelnyk@telesens.ua>
2024-11-22 10:53:33 +0200

    TS-149 Added single page template

---
+++ themes/twentytwentyfour/templates/single-page-template.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: single-page
+*/
+get_header();
+
+?>
+<div class="services-tree bg-white">
+    <div class="flex-col sub_parent_0">
+        <div class="prod_container">
+            <?php
+            echo get_post_field('post_content', get_queried_object_id());
+            ?>
+            <br>
+            <br>
+        </div>
+    </div>
+</div>
+<?php
+get_footer();
