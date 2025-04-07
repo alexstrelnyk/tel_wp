5f3cbd03784a672121926b229b785a895546a933
alexstrelnyk <o.strelnyk@telesens.ua>
2025-04-07 21:02:15 +0300

    [temp] Fixed global markup bug

--- themes/twentytwentyfour/templates/single-page-template.php
+++ themes/twentytwentyfour/templates/single-page-template.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 <div class="services-tree bg-white single-page-template" style="margin-bottom: 60px;">
     <div class="flex-col sub_parent_0">
-        <div class="prod_container">
+        <div class="prod_container single-page-template">
             <?php
             echo get_post_field('post_content', get_queried_object_id());
             ?>
