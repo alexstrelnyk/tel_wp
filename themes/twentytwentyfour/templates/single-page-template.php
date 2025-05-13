067e711b286708ee396d00de14a08ccbde2c5790
alexstrelnyk <o.strelnyk@telesens.ua>
2025-05-13 12:49:05 +0300

    TS-205 Fixed bugs

--- themes/twentytwentyfour/templates/single-page-template.php
+++ themes/twentytwentyfour/templates/single-page-template.php
@@ -5,7 +5,7 @@ Template Name: single-page
 get_header();
 
 ?>
-<div class="services-tree bg-white single-page-template" style="margin-bottom: 60px;">
+<div class="services-tree bg-soft-blue single-page-template" style="padding-bottom: 60px;">
     <div class="flex-col sub_parent_0">
         <div class="prod_container single-page-template">
             <?php
